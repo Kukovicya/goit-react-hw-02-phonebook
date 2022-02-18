73215ca39c257da0a2da99c27c130eb27a99fa1e
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2022-02-18 17:07:50 +0200

    maybe final (update css)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
+import Phonebook from "./Phonebook";
+import './reset.css'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template blin oi
-    </div>
+    <>
+    <Phonebook />
+    </>
   );
 };
